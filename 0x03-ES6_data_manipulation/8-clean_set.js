9e0a49f9154559ca6a24a7dccaaaf75f9c1d6c0f
nita youssef <nitayoussef3@gmail.com>
2024-08-24 10:07:38 +0000

    WER

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,25 @@
+/**
+ * function that returns a string of all the set values that start
+ * with a specific string (startString).
+ * @param {Set} set
+ * @param {String} startString
+ * @returns {String}
+ */
+export default function cleanSet(set, startString) {
+  if (
+    !startString || !(set instanceof Set)
+    || typeof startString !== 'string'
+    || !set
+  ) {
+    return '';
+  }
+  const result = [];
+  for (const elem of set) {
+    if (elem && elem.startsWith(startString) && typeof elem === 'string') {
+      const value = elem.slice(startString.length);
+
+      if (value && value !== elem) result.push(value);
+    }
+  }
+  return result.join('-');
+}
